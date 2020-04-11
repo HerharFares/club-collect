a1f88a6ed1376c506262292b9a7aecd791ee7068
Herhar Fares <herhar.fares@yandex.com>
2020-04-11 19:11:40 +0100

    adding csv_writer

--- tools.py
+++ tools.py
@@ -1,4 +1,5 @@
 from config import REPLACE_ITEMS
+import csv
 
 def replacing(string):
     """To convert the name of the universities from 
@@ -7,3 +8,22 @@ def replacing(string):
     for temp in REPLACE_ITEMS:
         string = string.replace(temp[0], temp[1])
     return string
+
+
+def csv_writer(file_name, row, data):
+    """This functoin writes data into csv file,
+    by speciying the rows head titles, and the data to put.
+    """
+    csvfile = open(file_name,"w")  # the csv file
+    csvwriter = csv.writer(csvfile, delimiter=',', quotechar='|',
+                           quoting=csv.QUOTE_MINIMAL)
+
+    # writing desciption row
+    csvwriter.writerow(row)
+
+    # writing the data
+    for item in data:
+        csvwriter.writerow(item)
+    
+    # close the file
+    csvfile.close()
