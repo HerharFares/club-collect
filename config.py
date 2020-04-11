46a34d4ff6054fafba96c86db10872034361d805
Herhar Fares <herhar.fares@yandex.com>
2020-04-11 18:57:53 +0100

    Updating the code

--- config.py
+++ config.py
@@ -1,2 +1,22 @@
+import os
+BASE_DIR = os.getcwd()
+
 UNIV_LINK_AR = "https://www.mesrs.dz/ar/universites"
 UNIV_LINK_FR = "https://www.mesrs.dz/fr/universites"
+
+REPLACE_ITEMS = [
+("technologie", "technology"),
+("Université", "University"),
+(" et de ", " and "),
+(" et des ", " and "),
+(" des ", " of "),
+(" de ", " of "),
+("d'", " of "),
+(" et ", " and "),
+(" le ", " "),
+(" la ", " "),
+(" les ", ""),
+(" l'", ""),
+("è", "e"),
+("é", "e"),
+]
